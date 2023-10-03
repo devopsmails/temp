aea918314007b735554069b1981cf218ada6be35
Devopsmails <119680288+devopsmails@users.noreply.github.com>
2023-10-03 12:26:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,5 +7,6 @@ resource "aws_instance" "gabby-ec2-1"{
     instance_type = "t2.micro"
     vpc = "aws_vpc.gabby_vpc.id"
     s3_bucket="aws_s3.bucket_name.id"
+    s3_bucket1="aws_s3.bucket_name1.id"
 }
 
