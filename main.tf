35113ccd1db5e8a54cc5618586b751c921ed88a0
Devopsmails <devopsmails1@gmail.com>
2023-10-03 12:41:05 +0530

    gkkfd

--- main.tf
+++ main.tf
@@ -1,13 +1,6 @@
 provdier "aws" {
     region = "us-east-1"
 }
-
-resource "aws_instance" "gabby-ec2-1"{
-    ami = "######"
-    instance_type = "t2.micro"
-    vpc = "aws_vpc.gabby_vpc.id"
-    s3_bucket="aws_s3.bucket_name.id"
-    s3_bucket1="aws_s3.bucket_name1.id"
-     s3_bucket2="aws_s3.bucket_name2.id"
+provdier "azure" {
+    region = "us-east-1"
 }
-
